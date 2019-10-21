61273a48a559023b4970ec41012f4c9c38f68eba
Student <example@example.com>
2019-10-21 15:28:16 -0700

    add cups to tablespoons conversion

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,17 @@
-// var literConversion = function(gallons) {return gallons/0.26417; } ;
-// var gallons = prompt("Enter the number of gallons.")
-// var literResult = literConversion(gallons);
-// alert(literResult);
+var literConversion = function(gallons) {return gallons/0.26417; } ;
+var gallons = prompt("Enter the number of gallons.")
+var literResult = literConversion(gallons).toFixed(2);
+alert(literResult);
 
-var Galloneq = function(liter)
-{
-  return (liter * .26417);
-};
-var liter = prompt("Enter the number of Liters.");
-var result = Galloneq(liter).toFixed(2);
-alert(result);
+// var Galloneq = function(liter)
+// {
+//   return (liter * .26417);
+// };
+// var liter = prompt("Enter the number of Liters.");
+// var result = Galloneq(liter).toFixed(2);
+// alert(result);
+
+var tbspEq = function(cups) {return cups * 16;};
+var cups = prompt("Enter the number of cups.");
+var tbsp = tbspEq(cups).toFixed(2);
+alert(tbsp);
